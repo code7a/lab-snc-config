00655ef0acf67b30171596fa6e6f99b208187878
code7a <115498258+code7a@users.noreply.github.com>
2022-11-29 14:34:18 -0500

    commit

--- download-bins-from-repo.sh
+++ download-bins-from-repo.sh
@@ -16,38 +16,38 @@
 
 dir=/illumio-repo/19.3.6/
 mkdir --parents $dir
-wget --timestamping --directory-prefix $dir $REPO/illumio-releases/19.3/GA%20Releases/19.3.6+H2/pce/pkgs/illumio-pce-19.3.6-17289.H2.x86_64.rpm
-wget --timestamping --directory-prefix $dir $REPO/illumio-releases/19.3/GA%20Releases/19.3.6+H2/pce/pkgs/UI/illumio-pce-ui-19.3.6.UI3-17283.x86_64.rpm
-wget --timestamping --directory-prefix $dir $REPO/illumio-releases/19.3/GA%20Releases/19.3.8/ven/bundle/illumio-ven-bundle-19.3.8-6520.tar.bz2
-wget --timestamping --directory-prefix $dir $REPO/illumio-releases-local/22.2/GA%20Releases/22.2.1/compatibility/illumio-release-compatibility-20-186.tar.bz2
+wget --quiet --timestamping --directory-prefix $dir $REPO/illumio-releases/19.3/GA%20Releases/19.3.6+H2/pce/pkgs/illumio-pce-19.3.6-17289.H2.x86_64.rpm
+wget --quiet --timestamping --directory-prefix $dir $REPO/illumio-releases/19.3/GA%20Releases/19.3.6+H2/pce/pkgs/UI/illumio-pce-ui-19.3.6.UI3-17283.x86_64.rpm
+wget --quiet --timestamping --directory-prefix $dir $REPO/illumio-releases/19.3/GA%20Releases/19.3.8/ven/bundle/illumio-ven-bundle-19.3.8-6520.tar.bz2
+wget --quiet --timestamping --directory-prefix $dir $REPO/illumio-releases-local/22.2/GA%20Releases/22.2.1/compatibility/illumio-release-compatibility-20-186.tar.bz2
 
 dir=/illumio-repo/21.2.7/
 mkdir --parents $dir
-wget --timestamping --directory-prefix $dir $REPO/illumio-releases/21.2/GA%20Releases/21.2.7/pce/pkgs/illumio-pce-21.2.7-656.c8.x86_64.rpm
-wget --timestamping --directory-prefix $dir $REPO/illumio-releases/21.2/GA%20Releases/21.2.7/pce/pkgs/UI/illumio-pce-ui-21.2.7.UI1-284.x86_64.rpm
-wget --timestamping --directory-prefix $dir $REPO/illumio-releases/21.2/GA%20Releases/21.2.5/ven/bundle/illumio-ven-bundle-21.2.5-8017.tar.bz2
-wget --timestamping --directory-prefix $dir $REPO/illumio-releases/21.2/GA%20Releases/21.2.5/compatibility/illumio-release-compatibility-15-161.tar.bz2
+wget --quiet --timestamping --directory-prefix $dir $REPO/illumio-releases/21.2/GA%20Releases/21.2.7/pce/pkgs/illumio-pce-21.2.7-656.c8.x86_64.rpm
+wget --quiet --timestamping --directory-prefix $dir $REPO/illumio-releases/21.2/GA%20Releases/21.2.7/pce/pkgs/UI/illumio-pce-ui-21.2.7.UI1-284.x86_64.rpm
+wget --quiet --timestamping --directory-prefix $dir $REPO/illumio-releases/21.2/GA%20Releases/21.2.5/ven/bundle/illumio-ven-bundle-21.2.5-8017.tar.bz2
+wget --quiet --timestamping --directory-prefix $dir $REPO/illumio-releases/21.2/GA%20Releases/21.2.5/compatibility/illumio-release-compatibility-15-161.tar.bz2
 
 dir=/illumio-repo/21.5.34/
 mkdir --parents $dir
-wget --timestamping --directory-prefix $dir $REPO/illumio-releases/21.5/GA%20Releases/21.5.34/pce/pkgs/illumio-pce-21.5.34-4.c8.x86_64.rpm
-wget --timestamping --directory-prefix $dir $REPO/illumio-releases/21.5/GA%20Releases/21.5.34/pce/pkgs/UI/illumio-pce-ui-21.5.34.UI1-1.x86_64.rpm
-wget --timestamping --directory-prefix $dir $REPO/illumio-releases/21.5/GA%20Releases/21.5.32/ven/bundle/illumio-ven-bundle-21.5.32-8533.tar.bz2
-wget --timestamping --directory-prefix $dir $REPO/illumio-releases/21.5/GA%20Releases/21.5.34/compatibility/illumio-release-compatibility-26-222.tar.bz2
+wget --quiet --timestamping --directory-prefix $dir $REPO/illumio-releases/21.5/GA%20Releases/21.5.34/pce/pkgs/illumio-pce-21.5.34-4.c8.x86_64.rpm
+wget --quiet --timestamping --directory-prefix $dir $REPO/illumio-releases/21.5/GA%20Releases/21.5.34/pce/pkgs/UI/illumio-pce-ui-21.5.34.UI1-1.x86_64.rpm
+wget --quiet --timestamping --directory-prefix $dir $REPO/illumio-releases/21.5/GA%20Releases/21.5.32/ven/bundle/illumio-ven-bundle-21.5.32-8533.tar.bz2
+wget --quiet --timestamping --directory-prefix $dir $REPO/illumio-releases/21.5/GA%20Releases/21.5.34/compatibility/illumio-release-compatibility-26-222.tar.bz2
 
 dir=/illumio-repo/22.2.30/
 mkdir --parents $dir
-wget --timestamping --directory-prefix $dir $REPO/illumio-releases/22.2/GA%20Releases/22.2.30/pce/pkgs/illumio-pce-22.2.30-631.c8.x86_64.rpm
-wget --timestamping --directory-prefix $dir $REPO/illumio-releases/22.2/GA%20Releases/22.2.30/pce/pkgs/UI/illumio-pce-ui-22.2.30.UI1-460.x86_64.rpm
-wget --timestamping --directory-prefix $dir $REPO/illumio-releases/22.2/GA%20Releases/22.2.30/ven/bundle/illumio-ven-bundle-22.2.30-9138.tar.bz2
-wget --timestamping --directory-prefix $dir $REPO/illumio-releases/22.2/GA%20Releases/22.2.30/compatibility/illumio-release-compatibility-25-219.tar.bz2
+wget --quiet --timestamping --directory-prefix $dir $REPO/illumio-releases/22.2/GA%20Releases/22.2.30/pce/pkgs/illumio-pce-22.2.30-631.c8.x86_64.rpm
+wget --quiet --timestamping --directory-prefix $dir $REPO/illumio-releases/22.2/GA%20Releases/22.2.30/pce/pkgs/UI/illumio-pce-ui-22.2.30.UI1-460.x86_64.rpm
+wget --quiet --timestamping --directory-prefix $dir $REPO/illumio-releases/22.2/GA%20Releases/22.2.30/ven/bundle/illumio-ven-bundle-22.2.30-9138.tar.bz2
+wget --quiet --timestamping --directory-prefix $dir $REPO/illumio-releases/22.2/GA%20Releases/22.2.30/compatibility/illumio-release-compatibility-25-219.tar.bz2
 
 dir=/illumio-repo/22.5.0.RC2/
 mkdir --parents $dir
-wget --timestamping --directory-prefix $dir $REPO/illumio-staging/22.5/GA%20Releases/22.5.0.RC2/pce/pkgs/illumio-pce-22.5.0-1002.c8.x86_64.rpm
-wget --timestamping --directory-prefix $dir $REPO/illumio-staging/22.5/GA%20Releases/22.5.0.RC2/pce/pkgs/UI/illumio-pce-ui-22.5.0.UI1-1149.x86_64.rpm
-wget --timestamping --directory-prefix $dir $REPO/illumio-staging/22.5/GA%20Releases/22.5.0.RC2/ven/bundle/illumio-ven-bundle-22.5.0-9615.tar.bz2
-wget --timestamping --directory-prefix $dir $REPO/illumio-releases/22.2/GA%20Releases/22.2.30/compatibility/illumio-release-compatibility-25-219.tar.bz2
+wget --quiet --timestamping --directory-prefix $dir $REPO/illumio-staging/22.5/GA%20Releases/22.5.0.RC2/pce/pkgs/illumio-pce-22.5.0-1002.c8.x86_64.rpm
+wget --quiet --timestamping --directory-prefix $dir $REPO/illumio-staging/22.5/GA%20Releases/22.5.0.RC2/pce/pkgs/UI/illumio-pce-ui-22.5.0.UI1-1149.x86_64.rpm
+wget --quiet --timestamping --directory-prefix $dir $REPO/illumio-staging/22.5/GA%20Releases/22.5.0.RC2/ven/bundle/illumio-ven-bundle-22.5.0-9615.tar.bz2
+wget --quiet --timestamping --directory-prefix $dir $REPO/illumio-releases/22.2/GA%20Releases/22.2.30/compatibility/illumio-release-compatibility-25-219.tar.bz2
 
 #bin cleanup
 
